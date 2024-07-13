37b6e9a5d70d133f1983041a6311af8c79672398
Manuel <manuel@milkyway.zone>
2024-07-13 21:52:28 +0200

    feat: add utility functions to access the objects used by juno to parse the data

--- types/cmd/context.go
+++ types/cmd/context.go
@@ -9,10 +9,13 @@ import (
 	"github.com/spf13/cobra"
 
 	"github.com/forbole/juno/v5/database"
+	"github.com/forbole/juno/v5/logging"
 	"github.com/forbole/juno/v5/modules"
 	modsregistrar "github.com/forbole/juno/v5/modules/registrar"
+	"github.com/forbole/juno/v5/node"
 	nodebuilder "github.com/forbole/juno/v5/node/builder"
 	"github.com/forbole/juno/v5/parser"
+	"github.com/forbole/juno/v5/types"
 	configtypes "github.com/forbole/juno/v5/types/config"
 )
 
@@ -23,18 +26,22 @@ const ContextKey = JunoContextKey("juno.context")
 // Context represents the context that will be
 // inject the cobra command use to run juno
 type Context struct {
-	cfg               *Config
-	junoCfg           *configtypes.Config
-	moduleInitialized bool
-	modules           map[string]modules.Module
-	home              string
+	cfg     *Config
+	junoCfg *configtypes.Config
+
+	// Fields initialized at runtime
+	home               string
+	logger             *logging.Logger
+	database           *database.Database
+	node               *node.Node
+	modulesInitialized bool
+	modules            []modules.Module
 }
 
 func NewContextFromConfig(cfg *Config) *Context {
 	return &Context{
-		cfg:               cfg,
-		junoCfg:           nil,
-		moduleInitialized: false,
+		cfg:     cfg,
+		junoCfg: nil,
 	}
 }
 
@@ -62,6 +69,14 @@ func GetContext(cmd *cobra.Command) *Context {
 	return ctx
 }
 
+func (ctx *Context) Home() string {
+	return ctx.home
+}
+
+func (ctx *Context) SetHome(home string) {
+	ctx.home = home
+}
+
 func (ctx *Context) GetConfigFilePath() string {
 	if ctx.home == "" {
 		panic("Can't get config file path, home path is not set")
@@ -70,11 +85,12 @@ func (ctx *Context) GetConfigFilePath() string {
 	return path.Join(ctx.home, "config.yaml")
 }
 
-// GetConfig returns the juno's config
+// GetConfig returns the cmd configuration
 func (ctx *Context) GetConfig() *Config {
 	return ctx.cfg
 }
 
+// GetJunoConfig returns the juno configuration parsed from the config file
 func (ctx *Context) GetJunoConfig() (*configtypes.Config, error) {
 	if ctx.junoCfg == nil {
 		configFilePath := ctx.GetConfigFilePath()
@@ -95,72 +111,200 @@ func (ctx *Context) GetJunoConfig() (*configtypes.Config, error) {
 	return ctx.junoCfg, nil
 }
 
+// GetLogger returns the juno logger configured as specified in the juno config
+func (ctx *Context) GetLogger() (logging.Logger, error) {
+	if ctx.logger == nil {
+		logger := ctx.cfg.GetParseConfig().GetLogger()
+		junoConfig, err := ctx.GetJunoConfig()
+		if err != nil {
+			return nil, err
+		}
+
+		err = logger.SetLogFormat(junoConfig.Logging.LogFormat)
+		if err != nil {
+			return nil, fmt.Errorf("error while setting logging format: %s", err)
+		}
+
+		err = logger.SetLogLevel(junoConfig.Logging.LogLevel)
+		if err != nil {
+			return nil, fmt.Errorf("error while setting logging level: %s", err)
+		}
+
+		ctx.logger = &logger
+	}
+
+	return *ctx.logger, nil
+}
+
+// GetAccountAddressParser returns the account address parser to be used
+func (ctx *Context) GetAccountAddressParser() types.AccountAddressParser {
+	return ctx.cfg.GetParseConfig().GetAccountAddressParser()
+}
+
+// GetTransactionFilter returns the transaction filter configured in the
+// parse configuration
+func (ctx *Context) GetTransactionFilter() types.TransactionFilter {
+	return ctx.cfg.GetParseConfig().GetTransactionFilter()
+}
+
+// GetMessageFilter returns the message filter configured in the
+// parse configuration
+func (ctx *Context) GetMessageFilter() (types.MessageFilter, error) {
+	junoConfig, err := ctx.GetJunoConfig()
+	if err != nil {
+		return nil, err
+	}
+
+	msgFilter := ctx.cfg.GetParseConfig().GetMessageFilterBuilder()(*junoConfig)
+	return msgFilter, nil
+}
+
+// GetEncodingConfig returns the encoding configuration
+func (ctx *Context) GetEncodingConfig() types.EncodingConfig {
+	return ctx.cfg.GetParseConfig().GetEncodingConfigBuilder()()
+}
+
+// GetDatabase returns the database used by juno to store the data
+func (ctx *Context) GetDatabase() (database.Database, error) {
+	if ctx.database == nil {
+		junoCfg, err := ctx.GetJunoConfig()
+		if err != nil {
+			return nil, err
+		}
+
+		logger, err := ctx.GetLogger()
+		if err != nil {
+			return nil, err
+		}
+
+		msgFilter, err := ctx.GetMessageFilter()
+		if err != nil {
+			return nil, err
+		}
+
+		// Create the database
+		databaseCtx := database.NewContext(
+			junoCfg.Database,
+			ctx.GetEncodingConfig(),
+			logger,
+			ctx.GetTransactionFilter(),
+			msgFilter,
+		)
+		db, err := ctx.cfg.GetParseConfig().GetDBBuilder()(databaseCtx)
+		if err != nil {
+			return nil, err
+		}
+		ctx.database = &db
+	}
+
+	return *ctx.database, nil
+}
+
+// GetNode returns the node from which we parse the data
+func (ctx *Context) GetNode() (node.Node, error) {
+	if ctx.node == nil {
+		junoCfg, err := ctx.GetJunoConfig()
+		if err != nil {
+			return nil, err
+		}
+
+		// Create the node
+		nodeCtx := nodebuilder.NewContext(ctx.GetEncodingConfig(), ctx.GetAccountAddressParser())
+		node, err := nodebuilder.BuildNode(junoCfg.Node, nodeCtx)
+		if err != nil {
+			return nil, fmt.Errorf("failed to start client: %s", err)
+		}
+
+		ctx.node = &node
+	}
+	return *ctx.node, nil
+}
+
+// GetAllModules returns all the modules that have been registred
+func (ctx *Context) GetAllModules() ([]modules.Module, error) {
+	if !ctx.modulesInitialized {
+		junoConfig, err := ctx.GetJunoConfig()
+		if err != nil {
+			return nil, err
+		}
+
+		logger, err := ctx.GetLogger()
+		if err != nil {
+			return nil, err
+		}
+
+		db, err := ctx.GetDatabase()
+		if err != nil {
+			return nil, err
+		}
+
+		node, err := ctx.GetNode()
+		if err != nil {
+			return nil, err
+		}
+
+		context := modsregistrar.NewContext(
+			*junoConfig,
+			ctx.GetEncodingConfig(),
+			db,
+			node,
+			logger,
+			ctx.GetAccountAddressParser(),
+		)
+		ctx.modules = ctx.cfg.GetParseConfig().GetRegistrar().BuildModules(context)
+		ctx.modulesInitialized = true
+	}
+
+	return ctx.modules, nil
+}
+
+// GetModule returns the module with the given name
+func (ctx *Context) GetModule(module string) (modules.Module, error) {
+	allModules, err := ctx.GetAllModules()
+	if err != nil {
+		return nil, err
+	}
+
+	for _, mod := range allModules {
+		if mod.Name() == module {
+			return mod, nil
+		}
+	}
+
+	return nil, fmt.Errorf("module %s not found", module)
+}
+
 // GetParserContext setups all the things that can be used to later parse the chain state
 func (ctx *Context) GetParseContext() (*parser.Context, error) {
-	parseConfig := ctx.cfg.GetParseConfig()
 	cfg, err := ctx.GetJunoConfig()
 	if err != nil {
 		return nil, err
 	}
 
 	// Setup the logging
-	logger := parseConfig.GetLogger()
-	err = logger.SetLogFormat(cfg.Logging.LogFormat)
-	if err != nil {
-		return nil, fmt.Errorf("error while setting logging format: %s", err)
-	}
-
-	err = logger.SetLogLevel(cfg.Logging.LogLevel)
-	if err != nil {
-		return nil, fmt.Errorf("error while setting logging level: %s", err)
-	}
-
-	// Get the account parser and filters
-	accountAddressParser := parseConfig.GetAccountAddressParser()
-	transactionFilter := parseConfig.GetTransactionFilter()
-	messageFilter := parseConfig.GetMessageFilterBuilder()(*cfg)
-
-	// Build the codec
-	encodingConfig := parseConfig.GetEncodingConfigBuilder()()
-	// Create the database
-	databaseCtx := database.NewContext(
-		cfg.Database,
-		encodingConfig,
-		logger,
-		transactionFilter,
-		messageFilter,
-	)
-	db, err := parseConfig.GetDBBuilder()(databaseCtx)
+	logger, err := ctx.GetLogger()
 	if err != nil {
 		return nil, err
 	}
 
-	// Create the node
-	nodeCtx := nodebuilder.NewContext(encodingConfig, accountAddressParser)
-	node, err := nodebuilder.BuildNode(cfg.Node, nodeCtx)
+	// Get the encoding config
+	encodingConfig := ctx.GetEncodingConfig()
+	db, err := ctx.GetDatabase()
 	if err != nil {
-		return nil, fmt.Errorf("failed to start client: %s", err)
+		return nil, err
 	}
 
-	// Build the modules
-	context := modsregistrar.NewContext(
-		*cfg,
-		encodingConfig,
-		db,
-		node,
-		logger,
-		accountAddressParser,
-	)
-	mods := parseConfig.GetRegistrar().BuildModules(context)
+	// Get the node
+	node, err := ctx.GetNode()
+	if err != nil {
+		return nil, err
+	}
+
+	mods, err := ctx.GetAllModules()
+	if err != nil {
+		return nil, err
+	}
+
 	registeredModules := modsregistrar.GetModules(mods, cfg.Chain.Modules, logger)
-
 	return parser.NewContext(*cfg, encodingConfig, node, db, logger, registeredModules), nil
-}
-
-func (ctx *Context) Home() string {
-	return ctx.home
-}
-
-func (ctx *Context) SetHome(home string) {
-	ctx.home = home
 }
