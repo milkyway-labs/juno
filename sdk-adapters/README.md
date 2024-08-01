ddf01b77ba7671f69c04ac34db3b274073944270
Manuel <manuel@milkyway.zone>
2024-08-01 18:04:27 +0200

    feat: add sdk codec adapter

---
+++ sdk-adapters/README.md
@@ -0,0 +1,3 @@
+# SDK Adapters
+
+This module provides a set of adapter to interact with the Cosmos SDK trough Juno.
---
+++ sdk-adapters/codec/adapter.go
@@ -0,0 +1,49 @@
+package codec
+
+import (
+	"github.com/cosmos/cosmos-sdk/codec"
+	codectypes "github.com/cosmos/cosmos-sdk/codec/types"
+	"github.com/cosmos/gogoproto/proto"
+	junocodec "github.com/forbole/juno/v5/cosmos-sdk/codec"
+	junocodectypes "github.com/forbole/juno/v5/cosmos-sdk/codec/types"
+	"google.golang.org/grpc/encoding"
+)
+
+type JunoCodecAdapater struct {
+	codec *codec.ProtoCodec
+}
+
+func NewJunoCodecAdapter(codec *codec.ProtoCodec) *JunoCodecAdapater {
+	return &JunoCodecAdapater{
+		codec: codec,
+	}
+}
+
+func (j *JunoCodecAdapater) MarshalJSON(o proto.Message) ([]byte, error) {
+	return j.codec.MarshalJSON(o)
+}
+
+func (j *JunoCodecAdapater) GetSdkCodec() *codec.ProtoCodec {
+	return j.codec
+}
+
+func (j *JunoCodecAdapater) GRPCCodec() encoding.Codec {
+	return j.codec.GRPCCodec()
+}
+
+func (j *JunoCodecAdapater) UnpackAny(any *junocodectypes.Any, iface interface{}) error {
+	sdkAny := codectypes.Any{
+		TypeUrl: any.TypeUrl,
+		Value:   any.Value,
+	}
+	return j.codec.UnpackAny(&sdkAny, iface)
+}
+
+func CastCodec(c junocodec.Codec) *JunoCodecAdapater {
+	protoCodec, ok := c.(*JunoCodecAdapater)
+	if !ok {
+		panic("invalid codec")
+	}
+	return protoCodec
+}
+
