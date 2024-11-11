433d74be7c6c80132050e8388bd3516cdb1507cb
rustcandy <gloria@milkyway.zone>
2024-11-11 21:20:00 +0900

    feat: remove tracking error count

--- prometheus/prometheus.go
+++ prometheus/prometheus.go
@@ -1,8 +1,6 @@
 package prometheus
 
 import (
-	"fmt"
-
 	"github.com/prometheus/client_golang/prometheus"
 )
 
@@ -31,63 +29,8 @@ var LatestIndexedHeightByWorker = prometheus.NewGaugeVec(
 	[]string{"worker_index"},
 )
 
-// ErrorsCount represents the Telemetry counter used to track the number of errors emitted
-var ErrorsCount = prometheus.NewCounter(
-	prometheus.CounterOpts{
-		Name: "juno_errors",
-		Help: "Total number of errors emitted.",
-	},
-)
-
-// RPCRequestErrorsCount represents the Telemetry counter used to track the number of RPC request errors
-var RPCRequestErrorsCount = prometheus.NewCounter(
-	prometheus.CounterOpts{
-		Name: "juno_rpc_errors",
-		Help: "Total number of errors occurred during RPC requests",
-	},
-)
-
-// DBOperationErrorsCount represents the Telemetry counter used to track the number of database operation errors
-var DBOperationErrorsCount = prometheus.NewCounter(
-	prometheus.CounterOpts{
-		Name: "juno_db_errors",
-		Help: "Total number of errors occurred during database operations",
-	},
-)
-
-// BlockErrorsCount represents the Telemetry counter used to track the number of errors per block
-var BlockErrorsCount = prometheus.NewCounterVec(
-	prometheus.CounterOpts{
-		Name: "juno_block_errors",
-		Help: "Total number of errors per block",
-	},
-	[]string{"block"},
-)
-
-// SignalRPCRequestError signal that a new rpc request error occurred
-func SignalRPCRequestError() {
-	RPCRequestErrorsCount.Inc()
-}
-
-// SignalDBOperationError signal that a new error occurred while interacting
-// with the database
-func SignalDBOperationError() {
-	DBOperationErrorsCount.Inc()
-}
-
-// SignalBlockError increments the error counter for the given block
-func SignalBlockError(blockHeight int64) {
-	blockStr := fmt.Sprintf("%d", blockHeight)
-	BlockErrorsCount.WithLabelValues(blockStr).Inc()
-	prometheus.MustRegister()
-}
-
 func init() {
 	prometheus.MustRegister(StartHeight)
 	prometheus.MustRegister(WorkersCount)
 	prometheus.MustRegister(LatestIndexedHeightByWorker)
-	prometheus.MustRegister(ErrorsCount)
-	prometheus.MustRegister(RPCRequestErrorsCount)
-	prometheus.MustRegister(DBOperationErrorsCount)
-	prometheus.MustRegister(BlockErrorsCount)
 }
